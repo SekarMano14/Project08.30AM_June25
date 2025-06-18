d563ed8d6951d342d213464e255a5803d730df36
SekarMano14 <sekarmano46@gmail.com>
2025-06-18 09:30:26 +0530

    Mano's code

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -3,6 +3,9 @@ package org.test;
 public class TestRunner {
 	public static void main(String[] args) {
 		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
 
 	}
 
